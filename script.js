dadda462db6cadd786a6ee00491700f9f388645f
void-the-droid <bhuwan626282@gmail.com>
2024-06-08 17:43:36 +0545

    end of initilization

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-// //Setting everthing
-// const addButton = document.querySelector('.add-btn');
-// const taskList = document.getElementById('task-list');
-// const container =document.querySelector('.container')
-// const infoModal =document.querySelector("#info");
-// const Add =document.querySelector("#the-btn")
-// const Selector =document.querySelector("select")
-// const categoriesList =document.querySelector("ul")
-// const workList =document.querySelector("#work")
-// const personalList =document.querySelector("#personal")
-// const shoppingList =document.querySelector("#shopping")
-// const otherList =document.querySelector("#other")
-// const allList =document.querySelector("#all")
-
+//Setting everthing
 
 //Importing 
 
 import {mainbody} from './body.js' 
 import {info} from './info-module.js' 
-
+// import {initializeApp} from "./initialize.js"
+// import { setupAddTask } from './Addtask.js'
+import { initializeApp } from './in.js'
 document.addEventListener("DOMContentLoaded",()=>{
-
+   
     // const container = document.querySelector(".container")
     mainbody()
      info()
    
+   
 })
 
-
+initializeApp()
 
 
